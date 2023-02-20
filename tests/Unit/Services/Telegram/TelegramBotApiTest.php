9f0a3e6d47d6ae2ea3886dbb1d98f88adb0a4324
Galina <galina89ruzhyk@mail.ru>
2023-02-20 21:29:01 +0800

    feat(test,provider,service): create TelegramBotApiContract

--- tests/Unit/Services/Telegram/TelegramBotApiTest.php
+++ tests/Unit/Services/Telegram/TelegramBotApiTest.php
@@ -4,14 +4,11 @@ declare(strict_types=1);
 
 use Illuminate\Support\Facades\Http;
 use Services\Telegram\TelegramBotApi;
+use Services\Telegram\TelegramBotApiContract;
 use Tests\TestCase;
 
 final class TelegramBotApiTest extends TestCase
 {
-    /**
-     * @test
-     * @return void
-     */
     public function test_send_message_success(): void
     {
         Http::fake([
@@ -21,5 +18,25 @@ final class TelegramBotApiTest extends TestCase
         $result = TelegramBotApi::sendMessage('', 1,'Testing');
 
         $this->assertTrue($result);
+    }
+
+    public function test_send_message_success_by_fake_instance(): void
+    {
+        TelegramBotApi::fake()
+            ->returnTrue();
+
+        $result = app(TelegramBotApiContract::class)::sendMessage('', 1, 'Testing');
+
+        $this->assertTrue($result);
+    }
+
+    public function test_send_message_fail_by_fake_instance(): void
+    {
+        TelegramBotApi::fake()
+            ->returnFalse();
+
+        $result = app(TelegramBotApiContract::class)::sendMessage('', 1, 'Testing');
+
+        $this->assertFalse($result);
     }
 }
