5a51d262b61d09130e395af8fed95705902ed0fb
Galina <galina89ruzhyk@mail.ru>
2023-02-22 00:52:53 +0800

    fix(): moved directory HasSlug, AuthRegistrar ...

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -2,12 +2,11 @@
 
 namespace App\Models;
 
-use App\Traits\Models\HasSlug;
-use App\Traits\Models\HasThumbnail;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Support\Traits\Models\HasSlug;
 
 /**
  * @mixin Builder
