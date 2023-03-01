816b69c850f65a7ab91a92c29540d29221fa8ae6
Galina <galina89ruzhyk@mail.ru>
2023-03-01 16:24:25 +0800

    fix(): RefreshCommand && HomeController

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Domain\Catalog\Models\Brand;
 use Domain\Catalog\Models\Category;
 use App\Models\Product;
+use Domain\Catalog\ViewModels\BrandViewModel;
 use Domain\Catalog\ViewModels\CategoryViewModel;
 use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Contracts\View\Factory;
@@ -17,7 +18,7 @@ class HomeController extends Controller
     {
         $categories = CategoryViewModel::make()->homePage();
 
-        $brands = Brand::query()->homePage()->get();
+        $brands = BrandViewModel::make()->homePage();
 
         $products = Product::query()->homePage()->get();
 
